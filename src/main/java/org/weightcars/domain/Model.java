27873160b6b34674d0a8f797f76f1efcda188c33
Benjamin Caure <benjamin.caure@gmail.com>
2018-11-30 22:08:37 +0100

    :white_check_mark: fix junit id generator

--- src/main/java/org/weightcars/domain/Model.java
+++ src/main/java/org/weightcars/domain/Model.java
@@ -17,8 +17,7 @@ public class Model implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
-    @SequenceGenerator(name = "sequenceGenerator")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "name")
