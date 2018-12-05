9d41ef8619bc202c44811f1d7e6cf93d3f9fec87
Benjamin Caure <benjamin.caure@gmail.com>
2018-12-06 00:26:31 +0100

    :art: add bars for weight & power file datasource :rotating_light: disable some tslint rules

--- src/main/webapp/app/app.tsx
+++ src/main/webapp/app/app.tsx
@@ -27,8 +27,7 @@ export interface ICar {
   realWeight?: number;
   officialWeight?: number;
   options?: string;
-  startDate?: Moment;
-  endDate?: Moment;
+  startDate?: string;
   model?: IModel;
 }
 
@@ -47,35 +46,44 @@ export class App extends React.Component<{}, IState> {
   componentDidMount() {}
 
   getCars() {
-    axios.get<ICar>(`api/cars?cacheBuster=${new Date().getTime()}`).then(response => this.setState({ cars: response.data as ICar[] }));
+    axios.get<ICar>(`api/cars?cacheBuster=${new Date().getTime()}`).then(response => {
+      this.setState({ cars: response.data as ICar[] });
+    });
   }
 
   render() {
     const paddingTop = '60px';
     let table;
     if (this.state.cars) {
-      table = this.state.cars.map(car => (
-        <Card className="jh-card">
-          <div className="row justify-content-around">
-            <span className="col">
-              {car.model.manufacturer.name} {car.model.name} {car.variant}
-            </span>
-            <span className="col badge badge-secondary">
-              {car.power}
-              Ch
-            </span>
-            <span className="col badge badge-primary">
-              {car.realWeight}
-              Kg (+
-              {car.realWeight - car.officialWeight})
-            </span>
-            <span className="col">{car.options}</span>
-            <span className="col">
-              {car.startDate}-{car.endDate}
-            </span>
-          </div>
-        </Card>
-      ));
+      table = this.state.cars.map(car => {
+        const powerStyle = {
+          width: `${car.power / 3}px`
+        };
+        const weightStyle = {
+          width: `${car.realWeight / 6}px`
+        };
+        const rowStyle = { display: 'inline' };
+        const year = car.startDate && car.startDate.substring(0, 4) !== '0001' ? car.startDate.substring(0, 4) : null;
+        return (
+          <Card key={car.id} className="jh-card">
+            <div className="row justify-content-around" style={rowStyle}>
+              <span className="col">{car.model.manufacturer.name}</span>
+              <span className="col" data-toggle="tooltip" title={car.options}>
+                {car.variant} ({year})
+              </span>
+              <span className="col badge badge-primary" style={weightStyle}>
+                {car.realWeight}
+                Kg (+
+                {car.realWeight - car.officialWeight})
+              </span>
+              <span className="col badge badge-danger" style={powerStyle}>
+                {car.power}
+                Ch
+              </span>
+            </div>
+          </Card>
+        );
+      });
     } else {
       table = <div>No cars</div>;
     }
