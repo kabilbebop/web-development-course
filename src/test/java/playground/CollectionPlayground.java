4fb467e972b45e47e936c17e6c58aa3a0d32a2f8
Benjamin Caure <bcaure@gmail.com>
2018-12-13 18:20:00 +0100

    :sparkles: junit playground

---
+++ src/test/java/playground/CollectionPlayground.java
@@ -0,0 +1,33 @@
+package playground;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.fail;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+
+public class CollectionPlayground {
+
+    @Test
+    public void testArrayList() {
+        List<String> strings = new ArrayList<>();
+        strings.add("a");
+        strings.add("b");
+        strings.add("c");
+        System.out.println(strings.get(0));
+        System.out.println(strings.get(1));
+        System.out.println(strings.get(2));
+
+        fail();
+    }
+
+    @Test
+    public void test() {
+        Integer i = 1;
+        assertEquals(2, i + i);
+        assertNotEquals(3, i + i);
+    }
+
+}
