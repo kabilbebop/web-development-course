cacb66d5e213dc07f6ead457aaff89f40153787f
Benjamin Caure <bcaure@gmail.com>
2019-01-07 14:33:24 +0100

    :lipstick: TD4 generator using LocalDate

--- src/test/java/playground/Generator.java
+++ src/test/java/playground/Generator.java
@@ -1,10 +1,9 @@
 package playground;
 
+import java.time.LocalDate;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.time.LocalDate;
 
 public class Generator {
 
