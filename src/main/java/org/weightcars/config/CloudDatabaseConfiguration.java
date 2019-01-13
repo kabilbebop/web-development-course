2c152f0026c00403e6087e53f6ac826854020505
Benjamin Caure <bcaure@gmail.com>
2019-01-13 21:58:12 +0100

    :construction_worker: enable cloud datasource connector

--- src/main/java/org/weightcars/config/CloudDatabaseConfiguration.java
+++ src/main/java/org/weightcars/config/CloudDatabaseConfiguration.java
@@ -1,9 +1,11 @@
 package org.weightcars.config;
 
 import io.github.jhipster.config.JHipsterConstants;
+import javax.sql.DataSource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.config.java.AbstractCloudConfig;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -13,9 +15,9 @@ public class CloudDatabaseConfiguration extends AbstractCloudConfig {
 
     private final Logger log = LoggerFactory.getLogger(CloudDatabaseConfiguration.class);
 
-//    @Bean
-//    public DataSource dataSource() {
-//        log.info("Configuring JDBC datasource from a cloud provider");
-//        return connectionFactory().dataSource();
-//    }
+    @Bean
+    public DataSource dataSource() {
+        log.info("Configuring JDBC datasource from a cloud provider");
+        return connectionFactory().dataSource();
+    }
 }
