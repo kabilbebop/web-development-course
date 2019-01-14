f6281d950f033c63c3699dc6dd8a3479849457e2
Benjamin Caure <bcaure@gmail.com>
2019-01-14 13:34:54 +0100

    :sparkles: solution TD4 exercise 5 (generator)

--- src/test/java/playground/Generator.java
+++ src/test/java/playground/Generator.java
@@ -11,18 +11,18 @@ public class Generator {
 
     @Test
     public void testGenerator1() {
-//        Film h2g2 = new Film("The hitchhiker's guide to the galaxy",
-//            50_000_000, LocalDate.of(2005, 8, 18));
-//        LOGGER.info(h2g2.toString());
+        Film h2g2 = new Film("The hitchhiker's guide to the galaxy",
+            50_000_000, LocalDate.of(2005, 8, 18));
+        LOGGER.info(h2g2.toString());
     }
 
     @Test
     public void testGenerator2() {
-//        Film snatch = new Film()
-//            .setName("Snatch")
-//            .setDate(LocalDate.of(2000, 11, 15))
-//            .setBudget(10_000_000);
-//        LOGGER.info(snatch.toString());
+        Film snatch = new Film()
+            .setName("Snatch")
+            .setDate(LocalDate.of(2000, 11, 15))
+            .setBudget(10_000_000);
+        LOGGER.info(snatch.toString());
     }
 
 }
@@ -33,5 +33,40 @@ class Film {
     Integer budget;
     LocalDate date;
 
+    public Film(String name, Integer budget, LocalDate date) {
+        this.name = name;
+        this.budget = budget;
+        this.date = date;
+    }
+
+    public Film() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Film setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Integer getBudget() {
+        return budget;
+    }
+
+    public Film setBudget(Integer budget) {
+        this.budget = budget;
+        return this;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public Film setDate(LocalDate date) {
+        this.date = date;
+        return this;
+    }
 }
 
