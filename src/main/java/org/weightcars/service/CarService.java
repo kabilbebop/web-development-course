4ce4875aef736f5a2b830f99484b0dd7394a6542
Benjamin Caure <benjamin.caure@gmail.com>
2019-02-17 23:22:53 +0100

    :sparkles: TD7 batch @Scheduled

--- src/main/java/org/weightcars/service/CarService.java
+++ src/main/java/org/weightcars/service/CarService.java
@@ -1,6 +1,9 @@
 package org.weightcars.service;
 
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.weightcars.domain.Car;
@@ -10,6 +13,8 @@ import org.weightcars.repository.CarRepository;
 @Service
 public class CarService {
 
+    private final static Logger LOGGER = LoggerFactory.getLogger(CarService.class);
+
     private final CarRepository carRepository;
 
     public CarService(CarRepository carRepository) {
@@ -17,6 +22,7 @@ public class CarService {
     }
 
     public Car refreshCarImage(Car car) {
+        LOGGER.info("refreshCarImage for {}", car.toString());
         String url = "http://www.carimagery.com/api.asmx/GetImageUrl?searchTerm=" + car.getModel().getManufacturer().getName();
         if (StringUtils.isNotEmpty(car.getModel().getName())) {
             url += "+" + car.getModel().getName();
@@ -31,5 +37,11 @@ public class CarService {
         carRepository.save(car);
 
         return car;
+    }
+
+    @Scheduled(fixedDelay = 24 * 3600 * 1000)
+    public void refreshAllCarImages() {
+        LOGGER.info("refresh all car images");
+        carRepository.findAll().forEach(this::refreshCarImage);
     }
 }
