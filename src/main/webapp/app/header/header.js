d56e31f722910403874b479a97f34d2f96df6da9
Benjamin Caure <bcaure@gmail.com>
2019-06-27 16:34:17 +0200

    :recycle: simplify front data structure :lipstick: imporve menu

--- src/main/webapp/app/header/header.js
+++ src/main/webapp/app/header/header.js
@@ -1,32 +1,28 @@
 import getTemplate from '/app/util.js';
 
-export class HeaderState {
-  menuOpen;
-}
-
-export class HeaderProps {
-  constructor(top10Click, menuOpen) {
-    this.top10Click = top10Click;
-    this.menuOpen = menuOpen;
-  }
-}
-
 export default class HeaderComponent extends HTMLElement {
-  state = {
-    menuOpen: false
-  };
+
+  get onMenuSelect() {
+    return this._onMenuSelect;
+  }
+
+  set onMenuSelect(value) {
+    this._onMenuSelect = value;
+  }
 
   constructor() {
     super();
     this.attachShadow({ mode: 'open' });
     getTemplate('/app/header/header.html').then(template => {
       this.shadowRoot.appendChild(template.content.cloneNode(true));
+      this.shadowRoot.querySelector('.top10-weight').addEventListener('click', () => this._onMenuSelect('.top10-weight'));
+      this.shadowRoot.querySelector('.top10-power').addEventListener('click', () => this._onMenuSelect('.top10-power'));
+      this.shadowRoot.querySelector('.top10-ratio').addEventListener('click', () => this._onMenuSelect('.top10-ratio'));
     });
+
+    this._onMenuSelect = function() {}; // callback defined by parent
   }
 
-  toggleMenu = () => {
-    this.setState({ menuOpen: !this.state.menuOpen });
-  };
 }
 
 customElements.define('header-component', HeaderComponent);
--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -2,126 +2,150 @@ const FILTER_INPUT_TIMEOUT = 500;
 
 let cars, filterInputTimeout, previousFilterValue, searchFilter;
 
-function refreshCars() {
-  /* fetch(`api/cars?cacheBuster=${new Date().getTime()}`).then(response => { */
-  cars = mockdata;/* response ? response.data : undefined; */
-  /* }); */
-  const appContent = document.querySelector('.app-content');
-  appContent.innerHTML = '';
-  let previousModel;
-  let carComponent;
-  let variants = [];
-  cars.forEach(car => {
-    if (!previousModel || previousModel !== car.model.name) {
-      if (previousModel && previousModel !== car.model.name) {
-        carComponent.variants = variants; // push property before creating new element
-      }
-      carComponent = document.createElement('car-component');
+function refreshCars(placeholder) {
+
+  placeholder.innerHTML = '';
+
+  cars.forEach(brand => {
+    brand.models.forEach(model => {
+      const carComponent = document.createElement('car-component');
       carComponent.classList.add('card');
-      carComponent.setAttribute('brand', car.model.manufacturer.name)
-      carComponent.setAttribute('model', car.model.name);
-      variants = [];
-    }
-    variants.push({
-      variant: car.variant,
-      year: car.startDate ? car.startDate.match(/\d{4}/g)[0] : undefined,
-      ratio: Math.round(car.realWeight / car.power),
-      weight: car.realWeight,
-      power: car.power
+
+      // we can push brand and model as attribute because they are strings
+      carComponent.setAttribute('brand', brand.name)
+      carComponent.setAttribute('model', model.name);
+
+      // push variants as property because it is an array
+      carComponent.variants = model.variants.map(variant => ({
+        variant: variant.name,
+        year: variant.startDate ? variant.startDate.match(/\d{4}/g)[0] : undefined,
+        ratio: Math.round(variant.realWeight / variant.power),
+        weight: variant.realWeight,
+        power: variant.power
+      }));
+
+      placeholder.appendChild(carComponent);
     });
-    appContent.appendChild(carComponent);
-    previousModel = car.model.name;
   });
-  carComponent.variants = variants; // push variants for last element 
 }
 
 function searchCars() {
-  const input = document.getElementById('searchFilter');
-  if (input.value !== previousFilterValue) {
-    clearTimeout(filterInputTimeout);
-    const timer = setTimeout(() => {
-      if (input.value && input.value.trim() !== '') {
-        fetch('api/cars/search/' + input.value).then(response => cars = response.data);
-      } else {
-        this.getCars();
+      const input = document.getElementById('searchFilter');
+      if (input.value !== previousFilterValue) {
+        clearTimeout(filterInputTimeout);
+        const timer = setTimeout(() => {
+          if (input.value && input.value.trim() !== '') {
+            fetch('api/cars/search/' + input.value).then(response => cars = response.data);
+          } else {
+            this.getCars();
+          }
+        }, FILTER_INPUT_TIMEOUT);
+        filterInputTimeout = timer;
+        previousFilterValue = input.value;
       }
-    }, FILTER_INPUT_TIMEOUT);
-    filterInputTimeout = timer;
-    previousFilterValue = input.value;
-  }
-}
+    }
 
 function top10Click(what) {
-  fetch(`api/cars/top/${what}/10`).then(response => cars = response.data);
-}
-
-
-const header = document.querySelector('header-component');
-// header. = () => {
-//   console.log('', header.shadowRoot)
-//   const top10Weight = header.shadowRoot.querySelector('.top10-weight');
-//   top10Weight.addEventListener('click', _event => console.log(_event) /* document.querySelector('car-component').setAttribute('model', 'TOTO') */);
-// };
-
-document.addEventListener('DOMContentLoaded', () => refreshCars());
-
-
-
-const mockdata = [{
-  "id": 4030000,
-  "variant": "A3 1,8T",
-  "power": 150.0,
-  "realWeight": 1213.0,
-  "officialWeight": 1145.0,
-  "options": "",
-  "startDate": "2002-01-01",
-  "imageUrl": null,
-  "image": null,
-  "model": {
-    "id": 403000,
-    "name": "A3",
-    "manufacturer": {
-      "id": 40,
-      "name": "AUDI"
+      fetch(`api/cars/top/${what}/10`).then(response => cars = response.data);
     }
-  }
-}, {
-  "id": 4030001,
-  "variant": "A3 2,0 TFSi SPORTBACK",
-  "power": 200.0,
-  "realWeight": 1485.0,
-  "officialWeight": 1410.0,
-  "options": "",
-  "startDate": "2010-01-01",
-  "imageUrl": null,
-  "image": null,
-  "model": {
-    "id": 403000,
-    "name": "A3",
-    "manufacturer": {
-      "id": 40,
-      "name": "AUDI"
-    }
-  }
-}, {
-  "id": 4030002,
-  "variant": "A3 2,0L TFSI SPORTBACK S-LINE",
-  "power": 190.0,
-  "realWeight": 1432.0,
-  "officialWeight": 1315.0,
-  "options": "",
-  "startDate": "2018-01-01",
-  "imageUrl": null,
-  "image": null,
-  "model": {
-    "id": 403000,
-    "name": "A3",
-    "manufacturer": {
-      "id": 40,
-      "name": "AUDI"
-    }
-  }
-},];
+
+document.addEventListener('DOMContentLoaded', () => {
+
+      // Header events binding
+      const header = document.querySelector('header-component');
+      header.onMenuSelect = menu => {
+        console.log('menu click!!', menu)
+      };
+
+      /* fetch(`api/cars?cacheBuster=${new Date().getTime()}`).then(response => { */
+      cars = mockdata;/* response ? response.data : undefined; */
+      /* }); */
+      const appContent = document.querySelector('.app-content');
+      refreshCars(appContent);
+    });
+
+
+
+  const mockdata = [{
+    "id": 40,
+    "name": "AUDI",
+    "models": [{
+      "id": 403000,
+      "name": "A3",
+      "variants": [{
+        "id": 4030000,
+        "name": "A3 1,8T",
+        "power": 150.0,
+        "realWeight": 1213.0,
+        "officialWeight": 1145.0,
+        "options": "",
+        "startDate": "2002-01-01",
+        "imageUrl": null,
+        "image": null,
+
+      }, {
+        "id": 4030001,
+        "name": "A3 2,0 TFSi SPORTBACK",
+        "power": 200.0,
+        "realWeight": 1485.0,
+        "officialWeight": 1410.0,
+        "options": "",
+        "startDate": "2010-01-01",
+        "imageUrl": null,
+        "image": null,
+      },
+      {
+        "id": 4030002,
+        "name": "A3 2,0L TFSI SPORTBACK S-LINE",
+        "power": 190.0,
+        "realWeight": 1432.0,
+        "officialWeight": 1315.0,
+        "options": "",
+        "startDate": "2018-01-01",
+        "imageUrl": null,
+        "image": null,
+      }]
+    },
+    {
+      "id": 403000,
+      "name": "A4",
+      "variants": [
+        {
+          "id": 4030003,
+          "name": "A4 3,0L TFSI SPORTBACK S-LINE",
+          "power": 300.0,
+          "realWeight": 1532.0,
+          "officialWeight": 1415.0,
+          "options": "",
+          "startDate": "2018-01-01",
+          "imageUrl": null,
+          "image": null,
+        }
+      ]
+    }]
+  },
+  {
+    "id": 41,
+    "name": "PEUGEOT",
+    "models": [{
+      "id": 403000,
+      "name": "308",
+      "variants": [
+        {
+          "id": 4030004,
+          "name": "308 GTI",
+          "power": 270.0,
+          "realWeight": 1232.0,
+          "officialWeight": 1215.0,
+          "options": "",
+          "startDate": "2018-01-01",
+          "imageUrl": null,
+          "image": null,
+
+        }
+      ]
+    }]
+  }];
 
 
 
