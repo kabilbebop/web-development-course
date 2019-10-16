82998e77d4b556ae2699673ec368f5a923d293a7
Benjamin Caure <bcaure@gmail.com>
2019-10-16 15:06:36 +0200

    Revert ":sparkles: solution TD1"
    This reverts commit d764d6c2ddc46c44e693aca191c8e579d8c42821.

---
+++ src/main/resources/public/components/util.js
@@ -0,0 +1,7 @@
+export default async function getTemplate(filepath) {
+  let response = await fetch(filepath);
+  let txt = await response.text();
+
+  let html = new DOMParser().parseFromString(txt, 'text/html');
+  return html.querySelector('template');
+}
--- src/main/resources/public/index.js
+++ src/main/resources/public/index.js
@@ -1,125 +1,99 @@
 
-const POWER_MAX = 1000; // max 1000hp, else overflow
-const WEIGHT_MAX = 2500; // max 2500kg, else overflow
-const RATIO_MAX = 1; // max 1kg/hp else overflow
+const BASE_URL = 'http://localhost:8745';
 
-const data = [{
-  "id": 160,
-  "name": "FORD",
-  "models": [
-    {
-      "id": 1601400,
-      "name": "MUSTANG",
-      "cars": [
-        {
-          "id": 16014004,
-          "name": "SHELBY GT500",
-          "power": 547.0,
-          "weight": 1789.0,
-          "startDate": "2011-01-01",
-          "imageUrl": "http://www.regcheck.org.uk/image.aspx/@Zm9yZCBtdXN0YW5n"
-        }
-      ]
-    }
-  ]
-},
-{
-  "id":40,
-  "name":"AUDI",
-  "models":[
-    {
-      "id":402000,
-      "name":"R8",
-      "cars":[
-        {
-          "id":40200016,
-          "name":"V8 S-STRONIC",
-          "power":430.0,
-          "weight":1666.0,
-          "startDate": "2008-01-01",
-          "imageUrl":"http://www.regcheck.org.uk/image.aspx/@QVVESSBSOCBWOCBSLVRST05JQw=="
-        }
-      ]
-    },
-    {
-      "id":401700,
-      "name":"RS4",
-      "cars":[
-        {
-          "id":4017005,
-          "name":"AVANT",
-          "power":420.0,
-          "weight":1728.0,
-          "startDate":"2006-01-01",
-          "imageUrl":"http://www.regcheck.org.uk/image.aspx/@YXVkaSByczQgYjc="
-        },
-        {
-          "id":4017006,
-          "name":"AVANT",
-          "power":450.0,
-          "weight":1867.0,
-          "startDate":"2013-01-01",
-          "imageUrl":"http://www.regcheck.org.uk/image.aspx/@YXVkaSByczQ="
-        }
-      ]
-    }
-  ]
-}];
+let loading, searchFilter;
 
+function renderCars(response) {
+
+  const placeholder = document.querySelector('.app-content');
+
+  placeholder.innerHTML = '';
+
+  response.forEach(brand => {
+    brand.models.forEach(model => {
+      const modelComponent = document.createElement('model-component');
+      modelComponent.classList.add('card');
+
+      // we can push brand and model as attribute because they are strings
+      modelComponent.setAttribute('brand', brand.name);
+      modelComponent.setAttribute('model', model.name);
+      modelComponent.setAttribute('image', model.cars[0].image);
+      modelComponent.setAttribute('image-link', model.cars[0].imageUrl);
+
+      // push variants as property because it is an array
+      modelComponent.cars = model.cars.map(car => ({
+        variant: car.variant,
+        year: car.startDate ? car.startDate.match(/\d{4}/g)[0] : undefined,
+        ratio: Math.round(car.realWeight * 10 / car.power) / 10,
+        weight: car.realWeight,
+        power: car.power,
+      }));
+
+      placeholder.appendChild(modelComponent);
+    });
+  });
+}
+
+function searchCars(value) {
+    showLoading();
+    const url = `${BASE_URL}/api/cars${value && value.trim() !== '' ? '/search/' + value : ''}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        renderCars(data);
+        hideLoading();
+      });
+}
+
+function top10Click(what) {
+  showLoading();
+  fetch(`${BASE_URL}/api/cars/top/${what}/10`)
+    .then(response => response.json())
+    .then(data => {
+      renderCars(data);
+      hideLoading();
+    });
+}
+
+function showLoading() {
+  document.querySelector('.loading').style.display = 'block';
+  loading = setTimeout(() => {
+    if (document.querySelector('.loading').innerText === '...') {
+      document.querySelector('.loading').innerText = '';
+    } else {
+      document.querySelector('.loading').innerText += '.';
+    }
+  }, 500); 
+}
+
+function hideLoading() {
+  clearTimeout(loading);
+  document.querySelector('.loading').innerText = '';
+  document.querySelector('.loading').style.display = 'none';
+}
 
 document.addEventListener('DOMContentLoaded', () => {
+  showLoading();
 
-  
-  const modelContents = document.querySelectorAll('.model-content');
-  let i = 0;
-  
-  data.forEach(brand => {
+  // Events binding
+  const header = document.querySelector('header-component');
+  header.onMenuSelect = menu => {
+    if (menu === 'home') {
+      searchCars();
+    } else {
+      top10Click(menu);
+    }
+  };
 
-    brand.models.forEach(model => {
+  header._onSearchChange = val => {
+    searchCars(val);
+  };
 
-      const modelContent = modelContents.item(i++);
-
-      modelContent.querySelector('.brand').innerText = brand.name;
-  
-      modelContent.querySelector('.model').innerText = model.name;
-    
-      modelContent.querySelector('.image-link').src = model.cars[0].imageUrl;
-    
-      modelContent.querySelector('.image').src = model.cars[0].imageUrl;
-  
-      const carContents = modelContent.querySelectorAll('.car-content');
-      
-      let j = 0;
-      model.cars.forEach(car => {
-        
-        const carContent = carContents.item(j++);
-        car = {
-          ...car,
-          year: car.startDate.match(/\d{4}/g)[0],
-          ratio: Math.round(car.weight * 10 / car.power) / 10,
-        };
-    
-        carContent.querySelector('.car').innerText = car.name;
-    
-        carContent.querySelector('.year').innerText = car.year;
-    
-        carContent.querySelector('.weight').innerText = car.weight;
-    
-        carContent.querySelector('.power').innerText = car.power;
-    
-        carContent.querySelector('.ratio').innerText = car.ratio;
-    
-        carContent.querySelector('.bar-power').style.width = (car.power * 100 / POWER_MAX) + '%';
-    
-        carContent.querySelector('.bar-weight').style.width = (car.weight * 100 / WEIGHT_MAX) + '%';
-    
-        carContent.querySelector('.bar-ratio').style.width = (RATIO_MAX * 100 / car.ratio) + '%';
-      });
-
+  // Initialize data
+  fetch(`${BASE_URL}/api/cars`)
+    .then(response => response.json())
+    .then(data => {
+      renderCars(data);
+      hideLoading();
     });
-
-  });
-
-
-
 });
