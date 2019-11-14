9bbc7404561e8c3f80e52cafc872ac6e7d94d3fb
Benjamin Caure <bcaure@gmail.com>
2019-11-14 16:03:04 +0100

    :recycle: move state to App component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,56 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import HeaderComponent from './components/header/header';
 import ModelComponent from './components/model/model';
 
-function App(props) {
 
-  const modelComponents = props.data.map(brand => <ModelComponent brand={ brand }></ModelComponent>);
+const BASE_URL = 'https://sport-cars.cfapps.io/';
+
+function menuClick(setData, menu) {
+    if (menu === 'home') {
+        searchCars(setData, null);
+    } else {
+        top10Click(setData, menu);
+    }
+}
+
+function searchCars(setData, value) {
+
+    const url = `${BASE_URL}/api/cars${value && value.trim() !== '' ? '/search/' + value : ''}`;
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+          setData(data);
+    });
+}
+
+function top10Click(setData, what) {
+
+    fetch(`${BASE_URL}/api/cars/top/${what}/10`)
+        .then(response => response.json())
+        .then(data => {
+          setData(data);
+    });
+}
+
+function App() {
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}/api/cars`)
+      .then(response => response.json())
+      .then(data => {
+          setData(data);
+      });
+  }, []);
+  
+
+  const modelComponents = data.map(brand => <ModelComponent key={ brand.id } brand={ brand }></ModelComponent>);
 
   return (
     <div className="App">
-      <header-component></header-component>
+      <header-component onMenuSelect={ menu => menuClick(setData, menu) } onSearchChange={ val => searchCars(setData, val) }></header-component>
       <section className="model-content">{ modelComponents }</section>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,14 +4,8 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const BASE_URL = 'https://sport-cars.cfapps.io/';
 
-// Initialize data
-fetch(`${BASE_URL}/api/cars`)
-.then(response => response.json())
-.then(data => {
-    ReactDOM.render(<App data={data} />, document.getElementById('root'));
-});
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
